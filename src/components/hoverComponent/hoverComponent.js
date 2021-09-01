a87ae816e71aa22c22d5ec38ac8b34cbfd38b2b3
unknown <franjseco@gmail.com>
2021-09-01 09:50:03 +0200

    deploying

---
+++ src/components/hoverComponent/hoverComponent.js
@@ -0,0 +1,11 @@
+import { Hover, Blockquote } from "./hoverComponent.styles.js";
+
+const HoverComponent = () => {
+  return (
+    <Hover className={"hover"}>
+      <Blockquote className={"quote"}> Olivia, Maria, Francisco </Blockquote>
+    </Hover>
+  );
+};
+
+export default HoverComponent;
